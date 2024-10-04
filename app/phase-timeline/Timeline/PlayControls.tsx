9b6e0d5501d0a64e0a2a6865986e1df5967d69cd
jialin <linao264590000@gmail.com>
2024-10-04 12:48:10 +0800

    feat: time input interaction

--- app/phase-timeline/Timeline/PlayControls.tsx
+++ app/phase-timeline/Timeline/PlayControls.tsx
@@ -1,37 +1,151 @@
-import React, { useCallback } from "react";
+"use client";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 type PlayControlsProps = {
   time: number;
-  setTime: (time: number) => void;
+  setTime: Dispatch<SetStateAction<number>>;
+};
+const STEP = 10;
+const MIN_VALUE = 0;
+const ALLOW_KEYS = [
+  "Backspace",
+  "Delete",
+  "ArrowUp",
+  "ArrowDown",
+  "Enter",
+  "Escape",
+  ".",
+  "-",
+];
+
+const isValidNumber = (value: string): boolean => {
+  const num = Number(value);
+  return !isNaN(num) && num >= MIN_VALUE;
 };
 
 export const PlayControls = ({ time, setTime }: PlayControlsProps) => {
-  // TODO: implement time <= maxTime
+  const [inputValue, setTemporaryTime] = useState(time.toString());
+  const inputRef = useRef<HTMLInputElement>(null);
+  const lastValidValue = useRef(time.toString());
+  const focusValue = useRef(time.toString());
+  const isCancelling = useRef(false);
+  const shouldSelectRef = useRef(false);
 
-  const onTimeChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setTime(Number(e.target.value));
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = e.target.value.replace(/^0+(?=\d)/, "");
+
+    let numericValue = parseFloat(newValue);
+
+    if (numericValue < 0) newValue = "0";
+    numericValue = Math.round(parseFloat(newValue));
+
+    // for negative situation
+    if (newValue === "") {
+      setTemporaryTime("");
+      return;
+    }
+
+    if (newValue === "" || isValidNumber(newValue)) {
+      setTemporaryTime(numericValue.toString());
+      // native input up-down button, directly update time
+      if (Math.abs(numericValue - Number(lastValidValue.current)) === STEP) {
+        setTime(Math.round(numericValue));
+        e.target.select();
+      }
+      lastValidValue.current = newValue.toString();
+    } else {
+      // back to origin
+      console.log("back to origin");
+      setTemporaryTime(lastValidValue.current);
+    }
+  };
+
+  const handleBlur = useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
+      if (!isCancelling.current) {
+        const newValue = Number(inputValue);
+        setTime(newValue);
+        lastValidValue.current = newValue.toString();
+      }
+      isCancelling.current = false;
     },
-    [setTime],
+    [inputValue, setTime]
   );
+
+  const handleFocus = useCallback((e: React.FocusEvent<HTMLInputElement>) => {
+    focusValue.current = e.target.value;
+    e.target.select();
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      // only allow these keys
+      if (
+        (e.key >= "0" && e.key <= "9") ||
+        (e.key >= "Numpad0" && e.key <= "Numpad9") ||
+        ALLOW_KEYS.includes(e.key)
+      ) {
+        if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+          e.preventDefault();
+          const stepChange = e.key === "ArrowUp" ? STEP : -STEP;
+          const newValue = Math.max(Number(inputValue) + stepChange, 0);
+          setTemporaryTime(newValue.toString());
+          setTime(newValue);
+          shouldSelectRef.current = true;
+        } else if (e.key === "Enter") {
+          inputRef.current?.blur();
+        } else if (e.key === "Escape") {
+          cancel();
+        }
+        return;
+      }
+
+      e.preventDefault();
+    },
+    [inputValue, setTime]
+  );
+
+  const cancel = useCallback(() => {
+    isCancelling.current = true;
+    setTemporaryTime(focusValue.current);
+    setTime(Number(focusValue.current));
+    inputRef.current?.blur();
+  }, [setTime]);
+
+  useEffect(() => {
+    if (shouldSelectRef.current && inputRef.current) {
+      inputRef.current.select();
+      shouldSelectRef.current = false;
+    }
+  }, [inputValue]);
 
   return (
     <div
-      className="flex items-center justify-between border-b border-r border-solid border-gray-700 
- px-2"
+      className="flex items-center text-white justify-between border-b border-r border-solid border-gray-700 px-2"
       data-testid="play-controls"
     >
       <fieldset className="flex gap-1">
         Current
         <input
+          ref={inputRef}
           className="bg-gray-700 px-1 rounded"
-          type="number"
           data-testid="current-time-input"
-          min={0}
+          min={MIN_VALUE}
           max={2000}
-          step={10}
-          value={time}
-          onChange={onTimeChange}
+          step={STEP}
+          type="number"
+          value={inputValue}
+          onInput={handleInput}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          onKeyDown={handleKeyDown}
         />
       </fieldset>
       -
